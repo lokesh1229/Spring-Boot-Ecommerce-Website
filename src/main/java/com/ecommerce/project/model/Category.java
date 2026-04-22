b56ad36899faf25e10692d85005715cab6db8766
Lokesh <lokeshkumarponna@gmail.com>
2026-04-22 14:09:07 +0530

    Upto Lombok and Handling exceptions

--- src/main/java/com/ecommerce/project/model/Category.java
+++ src/main/java/com/ecommerce/project/model/Category.java
@@ -4,37 +4,45 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 //@Entity
 @Entity(name = "Categories")  //--> Used in production grade applications to change table name as given as args
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Category {
 
-    public Category() {
-    }
+//    public Category() { --> We have used Lombok @Data @NoArgsConstructor AllArgsConstructor }
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long categoryId;
+
+    @NotBlank
     private String categoryName;
 
-    public Category(String categoryName, Long categoryId) {
-        this.categoryName = categoryName;
-        this.categoryId = categoryId;
-    }
-
-    public Long getCategoryId() {
-        return categoryId;
-    }
-
-    public void setCategoryId(Long categoryId) {
-        this.categoryId = categoryId;
-    }
-
-    public String getCategoryName() {
-        return categoryName;
-    }
-
-    public void setCategoryName(String categoryName) {
-        this.categoryName = categoryName;
-    }
+//    public Category(String categoryName, Long categoryId) {
+//        this.categoryName = categoryName;
+//        this.categoryId = categoryId;
+//    }
+//
+//    public Long getCategoryId() {
+//        return categoryId;
+//    }
+//
+//    public void setCategoryId(Long categoryId) {
+//        this.categoryId = categoryId;
+//    }
+//
+//    public String getCategoryName() {
+//        return categoryName;
+//    }
+//
+//    public void setCategoryName(String categoryName) {
+//        this.categoryName = categoryName;
+//    }
 }
